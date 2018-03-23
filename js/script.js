b16e999a32a553859c9a4f92e9379eb141eecbca
emozolev <emozolev@softrust.ru>
2018-03-23 18:14:22 +0300

    started services mobile

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $( document ).ready(function() {
 
     if( window.innerWidth < 991 ) {
         $('.carousel').carousel({
-            interval: false
+            'interval': false
         });
     };
 
