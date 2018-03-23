0771c6da1319273939fa01e89337c49e3c04ebd2
emozolev <emozolev@softrust.ru>
2018-03-23 14:38:35 +0300

    more fixes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 $( document ).ready(function() {
+
+    $(".carousel-inner").swipe( {
+        //Generic swipe handler for all directions
+        swipeLeft:function(event, direction, distance, duration, fingerCount) {
+            $(this).parent().carousel('prev');
+        },
+        swipeRight: function() {
+            $(this).parent().carousel('next');
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+        threshold:25
+    });
+
     $('.navbar-toggler').click(function(){
         $('.menu-button').toggleClass('toggler-invisible');
     });
