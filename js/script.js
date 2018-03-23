a84cfeaf7fe4d73ac0dfc240df220cef43030988
emozolev <emozolev@softrust.ru>
2018-03-23 14:49:29 +0300

    swipe test 1

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@ $( document ).ready(function() {
     $(".carousel-inner").swipe( {
         //Generic swipe handler for all directions
         swipeLeft:function(event, direction, distance, duration, fingerCount) {
-            $(this).parent().carousel('prev');
-        },
-        swipeRight: function() {
             $(this).parent().carousel('next');
         },
+        swipeRight: function() {
+            $(this).parent().carousel('prev');
+        },
         //Default is 75px, set to 0 for demo so any distance triggers swipe
-        threshold:0
+        threshold:0,
+        fingerReleaseThreshold: 50
     });
 
     $('.navbar-toggler').click(function(){
