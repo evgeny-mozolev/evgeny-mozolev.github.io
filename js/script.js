4517dd409fd1ce4ae5c494eadd800979cba1e33a
emozolev <emozolev@softrust.ru>
2018-04-03 18:05:43 +0300

    sliders

--- js/script.js
+++ js/script.js
@@ -1,24 +1,5 @@
 $( document ).ready(function() {
 
-    $(".carousel-inner").swipe( {
-        //Generic swipe handler for all directions
-        swipeLeft:function(event, direction, distance, duration, fingerCount) {
-            $(this).parent().carousel('next');
-        },
-        swipeRight: function() {
-            $(this).parent().carousel('prev');
-        },
-        //Default is 75px, set to 0 for demo so any distance triggers swipe
-        threshold:10
-    });
-
-    if( window.innerWidth < 991 ) {
-        $('.carousel').carousel({
-            'interval': false
-        });
-    };
-
-
     $('.navbar-toggler').click(function(){
         $('.menu-button').toggleClass('toggler-invisible');
     });
